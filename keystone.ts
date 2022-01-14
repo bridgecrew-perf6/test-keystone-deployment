035aa4c3cf087c708e7437496ecde465374bbde1
Yashdeep Bachhas <yashdeepbachhas93@gmail.com>
2022-01-14 21:01:44 +0530

    Basic keystone app

---
+++ keystone.ts
@@ -0,0 +1,22 @@
+import { config } from '@keystone-6/core';
+import 'dotenv/config';
+
+import { lists } from './schema';
+import { withAuth, session } from './auth';
+
+export default withAuth(
+  config({
+    db: {
+      provider: 'postgresql',
+      url: `${process.env.DATABASE_URL}`,
+    },
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    lists,
+    session,
+    graphql: {
+      playground: true,
+    },
+  })
+);
